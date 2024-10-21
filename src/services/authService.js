499ccb2af7bbd709826e9a77aa11db1bbec266f8
RosarioManny <rosario.emm47@gmail.com>
2024-10-21 17:39:30 -0400

    Fixed Error handling SignUp

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,18 @@
 import axios from 'axios'
 
-const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_UR
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
 
 const signup = async (formData) => {
   try {
-    const response = await axios.post('http://localhost300/users/signup', formData);
+    const response = await axios.post('http://localhost:3000/users/signup', formData);
     console.log(response)
-    
+    if(response.data.error) {
+      throw new Error(response.data.error)
+    }
     return response.data
-  } catch (err) {
-    console.log(err);
-    throw err;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 };
 
